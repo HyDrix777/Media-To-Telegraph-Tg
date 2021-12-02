dc53d50b6f429eb755fdc497acdd4fbaa15e3203
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2021-12-02 20:59:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -10,7 +10,7 @@ from telegraph import upload_file
 async def start(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"Hello {message.from_user.first_name},\n<b>I'm a Telegram To Telegra.ph Image/Video Uploader Bot. \n Created By @MaI_BotS</b> \n  Do /help For More",
+        text=f"𝗛𝗲𝗹𝗹𝗼 {message.from_user.first_name},\n<b>𝗜'𝗺 𝗮 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗧𝗼 Telegra.ph 𝗜𝗺𝗮𝗴𝗲/𝗩𝗶𝗱𝗲𝗼 𝗨𝗽𝗹𝗼𝗮𝗱𝗲𝗿 𝗕𝗼𝘁. \n 𝗢𝘄𝗻𝗲𝗿 By @HydraLivegrambot</b> \n  Do /help 𝗙𝗼𝗿 𝗺𝗼𝗿𝗲",
         reply_to_message_id=message.message_id
     )
 
@@ -18,7 +18,7 @@ async def start(client, message):
 async def help(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"<b> Send Me Any Video Or Photo I'll Upload It Into Telegra.ph. \n Created By @MaI_BotS</b>",
+        text=f"<b> 𝗦𝗲𝗻𝗱 𝗠𝗲 𝗔𝗻𝘆 𝗩𝗶𝗱𝗲𝗼 𝗢𝗿 𝗣𝗵𝗼𝘁𝗼 𝗜'𝗹𝗹 𝗨𝗽𝗹𝗼𝗮𝗱 𝗜𝘁 𝗜𝗻𝘁𝗼 Telegra.ph. \n 𝗢𝘄𝗻𝗲𝗿 By @HydraLivegrambot</b>",
         reply_to_message_id=message.message_id
     )
     
@@ -29,7 +29,7 @@ async def getimage(client, message):
         os.makedirs(location)
     imgdir = location + "/" + str(message.chat.id) + "/" + str(message.message_id) +".jpg"
     dwn = await client.send_message(
-          text="<b>Downloading...</b>",
+          text="<b>𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗶𝗻𝗴...</b>",
           chat_id = message.chat.id,
           reply_to_message_id=message.message_id
           )          
@@ -37,11 +37,11 @@ async def getimage(client, message):
             message=message,
             file_name=imgdir
         )
-    await dwn.edit_text("<b>Uploading...</b>")
+    await dwn.edit_text("<b>𝗨𝗽𝗹𝗼𝗮𝗱𝗶𝗻𝗴...</b>")
     try:
         response = upload_file(imgdir)
     except Exception as error:
-        await dwn.edit_text(f"Oops Something Went Wrong\n{error} Contact @No_OnE_Kn0wS_Me")
+        await dwn.edit_text(f"𝗢𝗼𝗽𝘀 𝗦𝗼𝗺𝗲𝘁𝗵𝗶𝗻𝗴 𝗪𝗲𝗻𝘁 𝗪𝗿𝗼𝗻𝗴\n{error} Contact @HydraLivegrambot")
         return
     await dwn.edit_text(f"https://telegra.ph{response[0]}")
     try:
@@ -56,7 +56,7 @@ async def getvideo(client, message):
         os.makedirs(location)
     viddir = location + "/" + str(message.chat.id) + "/" + str(message.message_id) +".mp4"
     dwn = await client.send_message(
-          text="<b>Downloading...</b>",
+          text="<b>𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗶𝗻𝗴...</b>",
           chat_id = message.chat.id,
           reply_to_message_id=message.message_id
           )          
@@ -64,11 +64,11 @@ async def getvideo(client, message):
             message=message,
             file_name=viddir
         )
-    await dwn.edit_text("<b>Uploading...</b>")
+    await dwn.edit_text("<b>𝗨𝗽𝗹𝗼𝗮𝗱𝗶𝗻𝗴...</b>")
     try:
         response = upload_file(viddir)
     except Exception as error:
-        await dwn.edit_text(f"Oops Something Went Wrong\n{error} Contact @No_OnE_Kn0wS_Me")
+        await dwn.edit_text(f"𝗢𝗼𝗽𝘀 𝗦𝗼𝗺𝗲𝘁𝗵𝗶𝗻𝗴 𝗪𝗲𝗻𝘁 𝗪𝗿𝗼𝗻𝗴\n{error} Contact @HydraLivegrambot")
         return
     await dwn.edit_text(f"https://telegra.ph{response[0]}")
     try:
